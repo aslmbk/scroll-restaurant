b8472050afb659526995d85ae56ca8cfaa2f7970
aslmbk <assayf95@gmail.com>
2024-10-16 23:54:43 +0300

    change paths to the assets

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ export const foodItems = [
     name: "Dango",
     description:
       "Dango is a Japanese dumpling and sweet made from mochiko, related to mochi.",
-    model: "/models/Food_Dango.gltf",
+    model: "./models/Food_Dango.gltf",
     page: 1,
   },
   {
     name: "Maguro Nigiri",
     description:
       "Nigiri is a type of sushi made of a slice of raw fish over pressed vinegared rice.",
-    model: "/models/Food_MaguroNigiri.gltf",
+    model: "./models/Food_MaguroNigiri.gltf",
     page: 2,
   },
   {
     name: "Ramen",
     description:
       "Ramen is a Japanese noodle soup. It consists of Chinese wheat noodles served in a meat or (occasionally) fish-based broth, often flavored with soy sauce or miso, and uses toppings such as sliced pork, nori, menma, and scallions.",
-    model: "/models/Food_Ramen.gltf",
+    model: "./models/Food_Ramen.gltf",
     page: 3,
   },
   {
     name: "Salmon Roll",
     description:
       "A salmon roll is a type of sushi roll. It usually contains cucumber, avocado, and salmon. It can sometimes also include other ingredients such as mayonnaise, lettuce, and sesame seed.",
-    model: "/models/Food_SalmonRoll.gltf",
+    model: "./models/Food_SalmonRoll.gltf",
     page: 4,
   },
 ];
